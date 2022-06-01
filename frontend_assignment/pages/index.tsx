3bfa89c7d72cad91cb57b46c221ef045f4f81239
amulya00 <amulya.kr00@gmail.com>
2022-06-01 22:13:22 +0530

    final

--- frontend_assignment/pages/index.tsx
+++ frontend_assignment/pages/index.tsx
@@ -1,13 +1,18 @@
 import detectEthereumProvider from "@metamask/detect-provider"
 import { Strategy, ZkIdentity } from "@zk-kit/identity"
 import { generateMerkleProof, Semaphore } from "@zk-kit/protocols"
-import { providers } from "ethers"
-import Head from "next/head"
+import { providers,Contract,utils } from "ethers"
 import React from "react"
 import styles from "../styles/Home.module.css"
+import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from '@hookform/resolvers/yup';
+import Greeter from "artifacts/contracts/Greeters.sol/Greeters.json"
+
 
 export default function Home() {
     const [logs, setLogs] = React.useState("Connect your wallet and greet!")
+    const [event,setevent]= React.useState("")
 
     async function greet() {
         setLogs("Creating your Semaphore identity...")
@@ -23,9 +28,15 @@ export default function Home() {
         const identity = new ZkIdentity(Strategy.MESSAGE, message)
         const identityCommitment = identity.genIdentityCommitment()
         const identityCommitments = await (await fetch("./identityCommitments.json")).json()
-
+        const prov = new providers.JsonRpcProvider("http://localhost:8545")
         const merkleProof = generateMerkleProof(20, BigInt(0), identityCommitments, identityCommitment)
-
+        // initializing greeter contract 
+        const contract = new Contract("0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",Greeter.abi,prov)
+        // Calling event listener function using ethersjs library . parsing bytes32 to string usign ethers
+        // using react usestate to store the event
+        contract.on('NewGreeting', (greeting: string) => {
+            setevent(utils.parseBytes32String(greeting))
+         })
         setLogs("Creating your Semaphore proof...")
 
         const greeting = "Hello world"
@@ -55,28 +66,59 @@ export default function Home() {
 
             setLogs(errorMessage)
         } else {
-            setLogs("Your anonymous greeting is onchain :)")
+            setLogs("Success")
         }
     }
+    // Interface of forminput , used from reactform hook
+    interface Iforminput {
+        firstName: string;
+        age: number;
+        address: string;
+    }
+// yup schema for name, age,address , declaring there types and making them required
+    const schema = yup.object({
+        firstName: yup.string().required(),
+        age: yup.number().positive().integer().required(),
+        address: yup.string().required()
+      }).required();
+
+      const {register,handleSubmit,formState: {errors}} = useForm<Iforminput>({
+        resolver: yupResolver(schema)
+    });
+
+    const onSubmit: SubmitHandler<Iforminput> = data => console.log(JSON.stringify(data));
 
     return (
         <div className={styles.container}>
-            <Head>
-                <title>Greetings</title>
-                <meta name="description" content="A simple Next.js/Hardhat privacy application with Semaphore." />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+
 
             <main className={styles.main}>
-                <h1 className={styles.title}>Greetings</h1>
+            <div style = {{border: "2px solid red", padding: 10, width: 600,marginTop: 10}}>
+                    <h1 style = {{marginLeft: 100}}>Your event goes here</h1>
+    
+                <div className = {styles.logs}> {event}</div>
 
-                <p className={styles.description}>A simple Next.js/Hardhat privacy application with Semaphore.</p>
-
-                <div className={styles.logs}>{logs}</div>
-
+                </div>
                 <div onClick={() => greet()} className={styles.button}>
                     Greet
                 </div>
+                <div className={styles.logs}>{logs}</div>
+            <form className={styles.formstyle} onSubmit = {handleSubmit(onSubmit)}>
+                <label> Name </label>
+                <input  {...register("firstName")}/>
+                <p>{errors.firstName?.message}</p>
+                <label> Age </label>
+                <input  {...register("age")}/>
+                <p>{errors.age?.message}</p>
+                <label>Address</label>
+                <input  {...register("address")}/>
+                <p>{errors.address?.message}</p>
+                <input className={styles.submitstyle} type="submit" />
+
+            </form>
+              
+
+              
             </main>
         </div>
     )
